66945e64e28c6d10aa3197d47771365ff2869934
Matt Lilley <thetrime@gmail.com>
2018-08-12 16:36:53 +0100

    Keep track of tasks in a todo

--- computer.pl
+++ computer.pl
@@ -12,11 +12,17 @@
 
 computer:-
 	on_signal(term, _, halt),
+	init_sphinx(computer, computer),
+	main_loop.
+
+main_loop:-
 	wait_for_keyword(computer),
 	listen_for_utterance(UtteranceTokens, Confidence),
 	writeln(Confidence),
 	parse_utterance(UtteranceTokens, Command),
-	effect_command(Command).
+	effect_command(Command),
+	halt,
+	main_loop.
 
 effect_command(Command):-
 	writeln(effect(Command)).
--- sphinx.c
+++ sphinx.c
@@ -68,13 +68,6 @@ foreign_t wait_for_keyword(term_t Keyword)
    {
       int32 bytes_read = ad_read(microphone, buffer, BUFSIZE);
       assert(bytes_read >= 0);
-      /*
-      if (bytes_read == 0)
-      {
-	 //sleep_msec(200);
-	 continue;
-      }
-      */
       ps_process_raw(ps, buffer, bytes_read, FALSE, FALSE);
       const char* hypothesis = ps_get_hyp(ps, &score);
       if (hypothesis != NULL && strcasecmp(hypothesis, keyword) == 0)
@@ -98,13 +91,15 @@ foreign_t listen_for_utterance(term_t Tokens, term_t Score)
    int32 score;
    int rc;
    ps_set_search(ps, "grammar");
-   int rv = ps_start_utt(ps);
+   int rv = ps_start_utt(ps);   
    int started_speaking = 0;
+   // This file can be played back using a command like: aplay -f S16_LE -r 16000 /tmp/retry.raw 
+   FILE* retry_buffer = fopen("/tmp/retry.raw", "wb");
    while (1)
    {
-      int32 bytes_read = ad_read(microphone, buffer, BUFSIZE);
-      assert(bytes_read >= 0);
-      rv = ps_process_raw(ps, buffer, bytes_read, FALSE, FALSE);
+      int32 samples_read = ad_read(microphone, buffer, BUFSIZE);
+      assert(samples_read >= 0);
+      rv = ps_process_raw(ps, buffer, samples_read, FALSE, FALSE);
       int is_speaking_now = ps_get_in_speech(ps);
       if (!started_speaking && is_speaking_now)
       {
@@ -116,46 +111,75 @@ foreign_t listen_for_utterance(term_t Tokens, term_t Score)
 	 // Looks like that is it!
 	 break;
       }
+      if (started_speaking)
+      {
+	 // Log the utterances to disk in case we want to redo the recognition later
+	 // Each sample is 16 bits, remember
+	 fwrite(buffer, samples_read, 2, retry_buffer);
+      }
       //sleep_msec(100);
    }
+   fclose(retry_buffer);
    rv = ps_end_utt(ps);
    hypothesis = ps_get_hyp(ps, &score);
    Sdprintf("*** utterance detected. Tokenizing...\n");
    rc = tokenize(hypothesis, Tokens);
-   Sdprintf("*** tokenzied!\n");
+   Sdprintf("*** tokenzied. Probability is %d, score is %d\n", ps_get_prob(ps), score);
    return PL_unify_integer(Score, score);
+}
+
+foreign_t init_sphinx(term_t Model, term_t Name)
+{
+   char* name;
+   char* model;
+   assert(PL_get_atom_chars(Name, &name));
+   assert(PL_get_atom_chars(Model, &model));
+   
+   /* Upper-case the name */
+   char* name_uc = PL_malloc(strlen(name));
+   for (char* source = name, *dest = name_uc; *source; source++, dest++)
+      *dest = toupper(*source);
+   Sdprintf("Hello %s\n", name_uc);
+
+   
+   char* dictionary = PL_malloc(strlen(model) + 5);
+   strcpy(dictionary, model);
+   strcat(dictionary, ".dic");
+
+   char* grammar = PL_malloc(strlen(model) + 3);
+   strcpy(grammar, model);
+   strcat(grammar, ".lm");
+
+   Sdprintf("ohai %s \n", dictionary);
+   /* initialize CMU Sphinx */
+   cmd_ln_t *config;
+   config = cmd_ln_init(NULL, ps_args(), TRUE,
+			"-hmm", MODELDIR "/en-us/en-us",
+			"-dict", dictionary,
+			"-kws_threshold", "1e-80",
+			NULL);
+   PL_free(dictionary);
+   assert(config != NULL);
+   ps = ps_init(config);
+
+   ps_set_keyphrase(ps, "kws", name_uc);
+   ps_set_lm_file(ps, "grammar", grammar);
+   PL_free(grammar);
+   PL_free(name_uc);
+   assert(ps != NULL);
+
+   microphone = ad_open_dev("plughw:1", 16000);
+   assert(microphone != NULL);
+
+   assert(ad_start_rec(microphone) >= 0);   
+   // FIXME: We do not free the cmd_ln anywhere. Might be OK for continuous usage
+   // FIXME: This cmd_ln_* stuff is deprecated in favour of a re-entrant API
+   PL_succeed;
 }
 
 install_t install_sphinx()
 {
    PL_register_foreign("wait_for_keyword", 1, wait_for_keyword, 0);
    PL_register_foreign("listen_for_utterance", 2, listen_for_utterance, 0);
-
-   /* Also, initialize CMU Sphinx */
-   cmd_ln_t *config;
-   config = cmd_ln_init(NULL, ps_args(), TRUE,
-			"-hmm", MODELDIR "/en-us/en-us",
-//			"-lm", MODEL ".lm",
-			"-dict", MODEL ".dic",
-//			"-kws", "computer",
-			"-kws_threshold", "1e-80",
-			NULL);
-   assert(config != NULL);
-
-   ps = ps_init(config);
-
-   ps_set_kws(ps, "kws", MODEL ".name");
-   ps_set_lm_file(ps, "grammar", MODEL ".lm");
-   
-   assert(ps != NULL);
-
-   microphone = ad_open_dev("plughw:1", 16000);
-   assert(microphone != NULL);
-
-   assert(ad_start_rec(microphone) >= 0);
-
-   // FIXME: We do not free the cmd_ln anywhere. Might be OK for continuous usage
-   // FIXME: This cmd_ln_* stuff is deprecated in favour of a re-entrant API
-      
-
+   PL_register_foreign("init_sphinx", 2, init_sphinx, 0);
 }
