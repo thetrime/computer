1558f4cd2ea7e84a4948d99ad1cc397d65ddab7e
Matt Lilley <thetrime@gmail.com>
2018-10-09 17:38:43 +0100

    Alaska?

--- computer.pl
+++ computer.pl
@@ -27,7 +27,7 @@ random_solution(Goal):-
         random_member(Goal, Goals).
 
 greeting('Aye, what now?').
-greeting('Ak, what is it?').
+greeting('Och, what is it?').
 greeting('Yeh, what is it?').
 greeting('Ah, I was asleep. What?'):-
         get_time(Time),
