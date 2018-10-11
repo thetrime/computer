2af80c60cf06953a115319c3b609762883f18460
Matt Lilley <thetrime@gmail.com>
2018-10-11 16:40:31 +0100

    Move API

--- schindler.pl
+++ schindler.pl
@@ -8,10 +8,10 @@
 computer:effect_command(parse_tree([add, Item, to, the, shopping, list])):-
         !,
         atom_codes(Item, Codes),
-        setup_call_cleanup(http_open('http://schindlerx.strangled.net:8080?key=matt',
+        setup_call_cleanup(http_open('http://schindlerx.strangled.net:8080/api/add_item?key=matt',
                                      Stream,
                                      [method(post),
                                       post(codes('timtext/plain', Codes))]),
-                           read_string(Stream, Reply, _),
+                           read_string(Stream, _, Reply),
                            close(Stream)),
         say(Reply, []).
